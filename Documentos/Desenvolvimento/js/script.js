8c778871fe5642e8ba1ee6e4c0f6b2de577587d8
Luan-Novais <luan.novaiss@outlook.com>
2020-09-21 08:28:38 -0300

    Finalizado header mobile and desktop

---
+++ Documentos/Desenvolvimento/js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('#nav-toggle').click(function(e){
+        e.preventDefault();
+        $(this).toggleClass('active');
+        $('.header-collapse').toggleClass('active')
+    });
+});
